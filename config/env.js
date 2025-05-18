5036cb14eb7e96c8491a67c37d72ab2a14c8f22d
Developer-Linus <linusmksu30@gmail.com>
2025-05-18 09:23:23 +0300

    Project setup: configured all the files needed and started the server

---
+++ config/env.js
@@ -0,0 +1,13 @@
+import { config } from "dotenv";
+
+// Get NODE_ENV or default to 'development'
+const env = process.env.NODE_ENV || 'development';
+
+// Construct the path like `.env.development.local` or `.env.production.local`
+const envPath = `.env.${env}.local`;
+
+// Load the appropriate .env file
+config({ path: envPath });
+
+// Export desired variables
+export const { PORT, NODE_ENV } = process.env;
